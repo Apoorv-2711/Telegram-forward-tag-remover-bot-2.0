7ada6b5e32705bdacdf906db529fdd82c82a6aef
Apoorv Jain <apoorvjain162@gmail.com>
2022-09-17 03:15:50 +0530

    Update commands.py

--- commands/commands.py
+++ commands/commands.py
@@ -1,18 +1,3 @@
-# ForwardTagRemoverBot - A Telegram Bot To Hide Forward Source
-#Copyright (C) 2020 by Rasak <https://github.com/Artis7eeR>
-#ForwardTagRemoverBot is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
-
-#ForwardTagRemoverBot is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup,ParseMode
 from config import Config
 
@@ -21,9 +6,6 @@ keyboard = [
 [
  InlineKeyboardButton("Source Code", url=Config.SOURCE)
 ],
-[
- InlineKeyboardButton("How To Create A Bot Like Me",url="https://youtu.be/swg6un2N4Fk")
-]
 ]
 
 #The Keyboard On Up👆
